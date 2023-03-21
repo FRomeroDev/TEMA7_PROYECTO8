96203af04dd58d3261414bf51160f2992fe178ac
FerDotCode <126758041+FerDotCode@users.noreply.github.com>
2023-03-21 13:00:29 +0100

    Update Ejercicio8_GIT_BBDD.java
    Hago un cambio en GIT

--- src/tema7Ejercicio8_GIT_BBDD/Ejercicio8_GIT_BBDD.java
+++ src/tema7Ejercicio8_GIT_BBDD/Ejercicio8_GIT_BBDD.java
@@ -7,6 +7,7 @@ public class Ejercicio8_GIT_BBDD {
 		
 		System.out.println("Hola Mundo con GIT y BBDD YUPPitooo");
 		System.out.println("Prueba 1 de Commit-Push");
+		System.out.println("Prueba 2 de Commit-Pull");
 
 		
 	}

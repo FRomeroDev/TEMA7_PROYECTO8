649382434b0c6ee57ee29eb5d9d0d31beea8610e
FerDotCode <f.romero.uni@gmail.com>
2023-03-21 12:46:28 +0100

    he añadido la frase de inicio "HolaMundo"
    Signed-off-by: FerDotCode <f.romero.uni@gmail.com>

---
+++ src/tema7Ejercicio8_GIT_BBDD/Ejercicio8_GIT_BBDD.java
@@ -0,0 +1,12 @@
+package tema7Ejercicio8_GIT_BBDD;
+
+public class Ejercicio8_GIT_BBDD {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Hola Mundo con GIT y BBDD YUPPIIII");
+		
+	}
+
+}
